7a79002932fd6b4f13e5ef6a64b3a279e5200de7
Ollie Batchelor <olliebatchelor@hotmail.com>
2023-08-08 12:44:25 +0100

    use correct url

--- app/api/route.ts
+++ app/api/route.ts
@@ -4,7 +4,7 @@ import { headers } from 'next/headers'
 export async function GET(request: NextRequest) {
     const cookieStore = headers();
 
-    const url = process.env.VERCEL_URL ? "https://$VERCEL_URL" : "http://localhost:3000";
+    const url = process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : "http://localhost:3000";
 
     const response = await fetch(`${url}/api/headerResponse`);
 
